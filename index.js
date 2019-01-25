3b921b37369541d729f3542f8999112a7e6ccf8f
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-25 05:56:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function doToElementsInArray(array,callback){
 
 // Add your changeCompletely() function here:
 function changeCompletely(elements,index,array){
-    array[index] = elements
+    array[index] = "antelope"
 }
